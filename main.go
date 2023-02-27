0a50d671cd129d6adcf2d6f91dc99959bec0038c
Tarun Thakur <tarun.t@vectoredge.io>
2023-02-27 19:02:05 +0530

    list drives

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	azidentity "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	"github.com/microsoftgraph/msgraph-sdk-go/models/odataerrors"
+)
+
+func main() {
+	// making an object containing the user creds
+	cred, err := azidentity.NewDeviceCodeCredential(&azidentity.DeviceCodeCredentialOptions{
+		ClientID: "94743c2d-6fc4-411e-a0f1-c88114c4a64e",
+		TenantID: "58656256-21c8-457c-b940-95a0df4e292b",
+
+		UserPrompt: func(ctx context.Context, message azidentity.DeviceCodeMessage) error {
+			fmt.Println(message.Message)
+			return nil
+		},
+	})
+	if err != nil {
+		fmt.Printf("Error creating credentials: %v\n", err)
+	}
+
+	// verifying the creds and getting a client to perform operations
+	client, err := msgraphsdk.NewGraphServiceClientWithCredentials(cred, []string{"Files.Read"})
+	if err != nil {
+		fmt.Printf("Error creating client: %v\n", err)
+		return
+	}
+
+	// getting the drive
+	result, err := client.Me().Drive().Get(context.Background(), nil)
+	if err != nil {
+		fmt.Printf("Error getting the drive: %v\n", err)
+		printOdataError(err)
+	}
+	fmt.Printf("Found Drive : %v\n", *result.GetId())
+}
+
+// omitted for brevity
+
+func printOdataError(err error) {
+	switch err.(type) {
+	case *odataerrors.ODataError:
+		typed := err.(*odataerrors.ODataError)
+		fmt.Printf("error:", typed.Error())
+		if terr := typed.GetError(); terr != nil {
+			fmt.Printf("code: %s", *terr.GetCode())
+			fmt.Printf("msg: %s", *terr.GetMessage())
+		}
+	default:
+		fmt.Printf("%T > error: %#v", err, err)
+	}
+}
